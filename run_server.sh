972b795a7ec7edc9f2502823a0a64bdff20fd2f6
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2025-05-22 10:59:01 +0200

    Delete run_server.sh

--- run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run server in background
-nohup go run main.go > server.log 2>&1 &
-echo "Server started in background"
-echo "PID: $!"
-echo "Logs: server.log"
-echo "For kontakt service, run in new terminal: cd kontakt && sudo make dev"
