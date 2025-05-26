4ec4444a5141f860437a58b729ef56b07a390630
Tomas Dvorak <contact.dvorak@gmail.com>
2025-05-26 11:39:32 +0200

    ff

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"gopkg.in/gomail.v2"
+	"ppve/admin" // Import the local admin package
 )
 
 type TripEntry struct {
@@ -81,26 +82,26 @@ func main() {
 		http.Redirect(w, r, "http://webportal:8080/", http.StatusFound)
 	}))
 	// Authentication routes
-	http.HandleFunc("/login", enableCORS(handleLogin))
-	http.HandleFunc("/logout", enableCORS(handleLogout))
+	http.HandleFunc("/login", enableCORS(admin.HandleLogin))
+	http.HandleFunc("/logout", enableCORS(admin.HandleLogout))
 
 	// Admin routes (protected)
-	http.HandleFunc("/admin", enableCORS(requireAdminAuth(handleAdmin)))
-	http.HandleFunc("/admin/cards", enableCORS(requireAdminAuth(handleAdminCards)))
+	http.HandleFunc("/admin", enableCORS(admin.RequireAdminAuth(admin.HandleAdmin)))
+	http.HandleFunc("/admin/cards", enableCORS(admin.RequireAdminAuth(admin.HandleAdminCards)))
 
-	http.HandleFunc("/admin/cards/", enableCORS(requireAdminAuth(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/admin/cards/", enableCORS(admin.RequireAdminAuth(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 		if strings.HasSuffix(path, "/toggle") {
-			handleAdminCardToggle(w, r)
+			admin.HandleAdminCardToggle(w, r)
 		} else if r.Method == "DELETE" {
-			handleAdminCardDelete(w, r)
+			admin.HandleAdminCardDelete(w, r)
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 		}
 	})))
 
 	// Public API to get cards for homepage
-	http.HandleFunc("/api/cards", enableCORS(handleGetCards))
+	http.HandleFunc("/api/cards", enableCORS(admin.HandleGetCards))
 
 	port := os.Getenv("PORT")
 	if port == "" {
